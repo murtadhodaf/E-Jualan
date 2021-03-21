28e702c42bfe3d40220c689dbcacbd3fbaa46712
murtadhodaf <murtadhodaf@gmail.com>
2021-03-21 09:56:20 +0700

    adding login user and admin

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\ChartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +28,20 @@ Route::get('/index.html', HomeComponent::class);
 route::get('/shop', ShopComponent::class);
 route::get('/cart', ChartComponent::class);
 route::get('/checkout', CheckoutComponent::class);
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+// For User
+Route::middleware(['auth:sanctum', 'verified', ])->group(function()
+{
+    Route::get('/user/dasboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+
+// For Admin
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function()
+{
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
+
