247b52b413ba8a8bf0571d23fe7a009710954897
murtadhodaf <murtadhodaf@gmail.com>
2021-03-22 03:18:11 +0700

    adding product details page1

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,6 +9,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/animate.css')}}"> 
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/flexslider.css')}}"> 
 	<link rel="stylesheet" type="text/css" href=" {{ asset ('assets/css/font-awesome.min.css') }}">
 	<link rel="stylesheet" type="text/css" href=" {{ asset ('assets/css/bootstrap.min.css') }}">
 	<link rel="stylesheet" type="text/css" href=" {{ asset ('assets/css/owl.carousel.min.css') }}">
