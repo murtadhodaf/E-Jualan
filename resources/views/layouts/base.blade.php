fbe55b3306d546a9e918163aff4e2e0fdb5321ca
murtadhodaf <murtadhodaf@gmail.com>
2021-03-22 18:41:18 +0700

    fixed minor typo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -168,7 +168,7 @@
 									<i class="fa fa-shopping-basket" aria-hidden="true"></i>
 									<div class="left-info">
 										@if (Cart::count() > 0)
-											<span class="index">{{Cart::count()}} item</span>
+											<span class="index">{{Cart::count()}} items</span>
 										@endif
 											<span class="title">CART</span>
 									</div>
