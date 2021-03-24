e7ae222494dbacca5ccaf0c3a26c2a16e0476bc4
murtadhodaf <murtadhodaf@gmail.com>
2021-03-24 13:24:38 +0700

    adding searcj category

--- resources/views/livewire/header-search-component.blade.php
+++ resources/views/livewire/header-search-component.blade.php
@@ -4,26 +4,14 @@
             <input type="text" name="search" value="" placeholder="Search here...">
             <button form="form-search-top" type="button"><i class="fa fa-search" aria-hidden="true"></i></button>
             <div class="wrap-list-cate">
-                <input type="hidden" name="product-cate" value="0" id="product-cate">
-                <a href="#" class="link-control">All Category</a>
+                <input type="hidden" name="product-cat" value="{{$product_cat}}" id="product-cate">
+                <input type="hidden" name="product-cat_id" value="{{$produuct_cat_id}}" id="product-cate">
+                <a href="#" class="link-control">{{str_split($product_cat,12)[0]}} </a>
                 <ul class="list-cate">
                     <li class="level-0">All Category</li>
-                    <li class="level-1">-Electronics</li>
-                    <li class="level-2">Batteries & Chargens</li>
-                    <li class="level-2">Headphone & Headsets</li>
-                    <li class="level-2">Mp3 Player & Acessories</li>
-                    <li class="level-1">-Smartphone & Table</li>
-                    <li class="level-2">Batteries & Chargens</li>
-                    <li class="level-2">Mp3 Player & Headphones</li>
-                    <li class="level-2">Table & Accessories</li>
-                    <li class="level-1">-Electronics</li>
-                    <li class="level-2">Batteries & Chargens</li>
-                    <li class="level-2">Headphone & Headsets</li>
-                    <li class="level-2">Mp3 Player & Acessories</li>
-                    <li class="level-1">-Smartphone & Table</li>
-                    <li class="level-2">Batteries & Chargens</li>
-                    <li class="level-2">Mp3 Player & Headphones</li>
-                    <li class="level-2">Table & Accessories</li>
+                    @foreach ($categories as $category)
+                        <li class="level-1" data-id="{{$category->id}}">{{$category->name}}</li>
+                    @endforeach
                 </ul>
             </div>
         </form>
